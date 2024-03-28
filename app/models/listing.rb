8ccd0f6070c3ac3bbfa2860c36a0020bffc25e30
ng <ng_zi_jie@hotmail.com>
2024-03-28 16:17:52 +0800

    pg search ongoing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,11 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_many_attached :photos
   has_many :bookings
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_country_and_state_and_subtitle_and_category_and_description,
+  against: [ :country, :title, :subtitle, :state, :category, :description ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
 end
