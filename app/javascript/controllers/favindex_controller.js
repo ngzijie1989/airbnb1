f94de6eed0d9425a8d0b225153562cfa096fd768
ng <ng_zi_jie@hotmail.com>
2024-03-29 09:24:12 +0800

    complete fav index

---
+++ app/javascript/controllers/favindex_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="favindex"
+export default class extends Controller {
+  static targets = ["favicon", "carddelete"]
+  static values = {id: Number}
+
+  connect() {
+  }
+
+  fire(e){
+    e.preventDefault();
+
+    const url = `/listings/${this.idValue}/addfavorites`
+    console.log(this.carddeleteTarget)
+
+    fetch(url, {
+      method: 'GET', 
+      headers: { "Accept": "application/json" },
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.status)
+      if (data.status == true) {
+        this.faviconTarget.innerHTML = `<i class="fa-solid fa-heart"></i>`
+        this.faviconTarget.classList.value = 'faviconred'
+      } else {
+        this.carddeleteTarget.outerHTML = ""
+        this.faviconTarget.innerHTML = `<i class="fa-regular fa-heart"></i>`
+        this.faviconTarget.classList.value = 'favicon'
+      }
+    })
+  }
+}
