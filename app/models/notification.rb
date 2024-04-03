49127bae47b8985f87bc06860aaa879c86d323ad
ng <ng_zi_jie@hotmail.com>
2024-04-03 20:35:43 +0800

    completed challenge

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,17 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :booking, optional: true
   belongs_to :listing, optional: true
+
+  def notification_header(message)
+    case message
+    when "new booking"
+      "New Booking. Please review the booking"
+    when "approve booking"
+      "Your booking has been approved"
+    when "reject booking"
+      "Your booking has been rejected"
+    else
+      "Unknown notification"
+    end
+  end
 end
